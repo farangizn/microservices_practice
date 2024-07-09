45a866d5c8c779b68103ec9227699ec4206715d5
farangizn <farangizxon2003@icloud.com>
2024-07-09 16:48:12 +0500

    minor changes

--- customer/src/test/java/org/example/customer/CustomerApplicationTests.java
+++ customer/src/test/java/org/example/customer/CustomerApplicationTests.java
@@ -1,12 +1,12 @@
 package org.example.customer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CustomerApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
